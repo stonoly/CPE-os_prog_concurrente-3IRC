adb0557c4e736ba43c5d1da94a4d2a137a07834f
stonoly <pierre.moly@gmail.com>
2025-02-04 16:45:43 +0100

    ex2 - Énigme

---
+++ tp1/ex2.c
@@ -0,0 +1,56 @@
+/* Fichier: ex1.c
+* Énigme
+* auteur: Pierre MOLY
+* Exercice 1
+*/
+
+#include "ex2.h"
+
+int reading_line(int tab[SIZE_TAB], int nbr_elem, int tab_out[SIZE_TAB]){
+    int index_tab_out = 0;
+    int elem = tab[0];
+    int cpt = 0;
+
+    for (int position_tab = 0; position_tab < nbr_elem + 1; position_tab++){
+        if (index_tab_out + 2 > SIZE_TAB ){
+            return -1;
+        } else {
+            if (tab[position_tab] == elem){
+                cpt++;
+            } else {
+                tab_out[index_tab_out] = cpt;
+                tab_out[index_tab_out + 1] = elem;
+                index_tab_out += 2;
+                elem = tab[position_tab];
+                cpt = 1;
+            }
+        }
+    }
+
+    return index_tab_out;
+}
+
+int main(){
+    int tab[SIZE_TAB] = {1, 1, 2, 3};
+    int tab_out[SIZE_TAB];
+    int nbr_elem = 4;
+
+    while(nbr_elem < SIZE_TAB){
+        nbr_elem = reading_line(tab, nbr_elem, tab_out);
+
+        if (nbr_elem == -1){
+            return 0;
+        }
+
+        printf("nbr_elem: %d\n", nbr_elem);
+
+        for (int i_display = 0; i_display < nbr_elem; i_display++){
+            tab[i_display] = tab_out[i_display];
+            printf("%d ", tab_out[i_display]);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ tp1/ex2.h
@@ -0,0 +1,8 @@
+#ifndef EX2_H
+#define EX2_H
+
+#define SIZE_TAB 40
+#include <stdio.h>
+
+
+#endif
