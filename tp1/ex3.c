f5d549f84659033455e3f07d6978187a9a11a90c
stonoly <pierre.moly@gmail.com>
2025-02-04 20:04:58 +0100

    Exercice 3 - Intersection of two lines

--- tp1/ex3.c
+++ tp1/ex3.c
@@ -1,48 +1,63 @@
 /* Fichier: ex3.c
-* Intersection of two lines
-* auteur: Pierre MOLY
-* Exercice 3
-*/
+ * Intersection of two lines
+ * auteur: Pierre MOLY
+ * Exercice 3
+ */
 
-#include "ex3.h"
+#include <stdio.h>
+#include <math.h>
+#include <float.h>
 
 float coefficient(float x1, float x2, float y1, float y2){
-    return (x2 == x1) ? INFINITY : (y2 - y1)/(x2 - x1);
+    return (x2 == x1) ? INFINITY : (y2 - y1) / (x2 - x1);
 }
 
-float ordonnée(float x, float y, float a){
-    return y - (a * x);
+float ordonnee(float x, float y, float a){
+    return (a == INFINITY) ? x : y - (a * x);
+}
+
+void intersection(float a1, float a2, float b1, float b2, float *x, float *y){
+    if (a1 == a2) {
+        if (b1 == b2) {
+            printf("Les droites sont confondues.\n");
+        } else {
+            printf("Les droites sont parallèles et n'ont pas d'intersection.\n");
+        }
+        *x = NAN;
+        *y = NAN;
+        return;
+    }
+
+    if (a1 == INFINITY) {
+        *x = b1;
+        *y = a2 * (*x) + b2;
+    } else if (a2 == INFINITY) {
+        *x = b2;
+        *y = a1 * (*x) + b1;
+    } else {
+        *x = (b2 - b1) / (a1 - a2);
+        *y = a1 * (*x) + b1;
+    }
 }
 
 int main(){
-    float P1x = 1;
-    float P1y = 2;
-    float P2x = 2;
-    float P2y = 0;
+    float P1x = 1, P1y = 3, P2x = 1, P2y = 3;
+    float P3x = -2, P3y = -1, P4x = 0, P4y = 3;
 
-    float P3x = 1;
-    float P3y = 3;
-    float P4x = 3;
-    float P4y = 2;
+    float a1 = coefficient(P1x, P2x, P1y, P2y);
+    float b1 = ordonnee(P1x, P1y, a1);
 
-    float a1;
-    float b1;
+    float a2 = coefficient(P3x, P4x, P3y, P4y);
+    float b2 = ordonnee(P3x, P3y, a2);
 
-    float a2;
-    float b2;
+    float x, y;
+    intersection(a1, a2, b1, b2, &x, &y);
 
-    a1 = coefficient(P1x, P2x, P1y, P2y);
-    b1 = ordonnée(P1x, P1y, a1);
-
-    a2 = coefficient(P3x, P4x, P3y, P4y);
-    b2 = ordonnée(P3x, P3y, a2);
-
-
-    printf("a1 = %f\n", a1);
-    printf("b1 = %f\n", b1);
-
-    printf("a2 = %f\n", a2);
-    printf("b2 = %f\n", b2);
+    printf("a1 = %f, b1 = %f\n", a1, b1);
+    printf("a2 = %f, b2 = %f\n", a2, b2);
+    if (!isnan(x) && !isnan(y)) {
+        printf("Intersection: x = %f, y = %f\n", x, y);
+    }
 
     return 0;
 }
